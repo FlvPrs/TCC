f8caea2ddca3ec75ee10278031c13e27317fd74b
Uiris <uirisgato@hotmail.com>
2017-11-02 23:59:23 -0200

    FMOD integração
    Integrei um projeto teste do FMOD ao UNITY

---
+++ TCC/Assets/_Scripts/ModelScripts/FMOD Audio/FMODEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODEvents : MonoBehaviour {
+
+	[FMODUnity.EventRef]
+	public string HarpaPuloEvent;
+	FMOD.Studio.EventInstance HarpaPulo;
+
+	void Start () {
+		HarpaPulo = FMODUnity.RuntimeManager.CreateInstance (HarpaPuloEvent);
+		HarpaPulo.set3DAttributes (FMODUnity.RuntimeUtils.To3DAttributes (gameObject));
+		HarpaPulo.start ();
+		print ("HarpaPuloEvent");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
