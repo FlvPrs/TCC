34345a66f0cff34e16ac881a511d55bc6ecf3648
ekh017 <erickhiga@hotmail.com>
2018-02-26 16:30:12 -0300

    Incrementado Codigo Puzzle Ato1
    Criei um teste com a logica do puzzle do ato 1 e ja aprece funcionar trocando a cor dos objetos quando em contato com o objeto certo.

---
+++ TCC/Assets/ControlePuzzleAto1.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlePuzzleAto1 : MonoBehaviour {
+	public bool polem;
+	public bool agua;
+	public bool polem_agua;
+	public int tipoDeRecurso;
+	public bool Completo;
+	private int Teste = 0;
+	private Color corFlor;
+	private Color Azul;
+	private Color Amarelo;
+	private Color Cinza;
+	private Color Verde;
+	private Color Inicial;
+	private Color Final;
+	private Renderer rend;
+	private float duracao = 3.0f;
+	// Use this for initialization
+	void Start () {
+		Completo = false;
+		tipoDeRecurso = 0;
+		rend = GetComponent<Renderer>();
+		Azul = Color.blue;
+		Amarelo = Color.yellow;
+		Cinza = Color.grey;
+		Verde = Color.green;
+		Final = Verde;
+		PuzzleFlor ();
+		rend.material.color = Inicial;
+	}
+		
+	void PuzzleFlor(){
+		if (agua) {
+			Inicial = Azul;
+			if (tipoDeRecurso == 1) {
+				Completo = true;
+			}
+		} else if (polem) {
+			Inicial = Amarelo;
+			if (tipoDeRecurso == 2) {
+				Completo = true;
+			}
+		} else if (polem_agua) {
+			Inicial = Cinza;
+			if (tipoDeRecurso == 1) {
+				polem = true;
+				polem_agua = false;
+			} else if (tipoDeRecurso == 2) {
+				agua = true;
+				polem_agua = false;
+			}
+		}
+		if (Completo && Teste == 0) {
+			Teste++;
+			MudançaDeCor ();
+		}
+
+	}
+	void MudançaDeCor(){
+		float lerp = Mathf.PingPong (Time.time, duracao) / duracao;
+		rend.material.color = Color.Lerp (Inicial, Final, lerp);
+	}
+	void OnTriggerEnter(Collider colisor){
+		if (colisor.name == "PlayerCollider") {
+			tipoDeRecurso = 2;
+			PuzzleFlor ();
+		} else if (colisor.name == "agua") {
+			tipoDeRecurso = 1;
+			PuzzleFlor ();
+			}
+	}
+}
