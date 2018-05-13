b3518f92e2bc7b0160711b46e9754fe6dff6f64f
fabio-marques <fabiocardosomarques@hotmail.com>
2018-05-13 16:26:20 -0300

    GeneralStuff
    Importei os audios da Planta Carnivora. Arrumei bug do audio listener que invertia a direção de audios 3D em algumas situações. Adicionei Fog no LVL01 pro terreno parecer estar bem longe. Arrumei algumas coisinhas no inverno.

--- TCC/Assets/VentiladorAutomatico_Ctrl.cs
+++ TCC/Assets/VentiladorAutomatico_Ctrl.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class VentiladorAutomatico_Ctrl : MonoBehaviour {
 
+	GameObject vento;
+
+	public float auto_OnOffTimer = 2f;
+	[Range(0.01f, 10f)]
+	public float auto_OnOffRatio = 1f; 	//É multiplicado por auto_onOffTimer. O resultado indica quanto tempo ficará ligada.
+										//1 significa o mesmo tempo ligado e desligado. 
+										//2 significa que ficará ligado 2 vezes mais do que desligado. 
+										//0.5 significa que ficará 2 vezes mais desligado do que ligado.
+	public float delayAutoBy = 0f;
+	float delayTimer;
+	bool canStartVentiladorAutomatico;
+	float ventiladorCooldown;
+
 	// Use this for initialization
 	void Start () {
-		
+		vento = transform.GetChild (0).gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (delayTimer <= 0f) {
+			ventiladorCooldown += Time.deltaTime;
+			if (ventiladorCooldown <= auto_OnOffRatio * auto_OnOffTimer || auto_OnOffTimer == 0f) {
+				vento.SetActive(true);
+			} else if (ventiladorCooldown <= auto_OnOffTimer + (auto_OnOffRatio * auto_OnOffTimer)) {
+				vento.SetActive(false);
+			} else {
+				ventiladorCooldown = 0f;
+			}
+		} else {
+			delayTimer -= Time.deltaTime;
+		}
 	}
 }
